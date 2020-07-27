779ba72be73faa3fd91977e17b6f050291bead33
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2020-07-27 04:33:42 +0200

    fine tuning nozzle/probe offset

--- Configuration.h
+++ Configuration.h
@@ -978,7 +978,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44.3, -7.2, -3.7 }
+#define NOZZLE_TO_PROBE_OFFSET { -44.3, -7.2, -3.63 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.

ce8b1f0e346989afff66df73a01cc9879b6a83e1
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2020-08-02 20:03:45 +0200

    enable REPORT_FAN_CHANGE

--- Configuration_adv.h
+++ Configuration_adv.h
@@ -3109,7 +3109,7 @@
 //#define M114_REALTIME       // Real current position based on forward kinematics
 //#define M114_LEGACY         // M114 used to synchronize on every call. Enable if needed.
 
-//#define REPORT_FAN_CHANGE   // Report the new fan speed when changed by M106 (and others)
+#define REPORT_FAN_CHANGE   // Report the new fan speed when changed by M106 (and others)
 
 /**
  * Set the number of proportional font spaces required to fill up a typical character space.

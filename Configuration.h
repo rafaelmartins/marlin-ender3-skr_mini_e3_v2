d494fd1fd9aba707872004b395be1f7b3ae4e39d
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2020-09-15 16:41:50 +0200

    update default probe offset Z axis
    now I'm using a ruthex 0.4 nozzle instead of the default one, and
    I'll probably stick with those for the time being. however, this needs
    to be calibrated any time I change the nozzle, because the positioning
    of bltouch is not precise, and the nozzle torque isn't either. but I'll
    still update the config because I like to reset default setting
    sometimes.

--- Configuration.h
+++ Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44.3, -7.2, -3.65 }
+#define NOZZLE_TO_PROBE_OFFSET { -44.3, -7.2, -3.85 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.

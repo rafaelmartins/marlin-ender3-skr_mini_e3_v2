22a73c43d907aa95e4e368727204a1413ecef560
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2023-09-04 01:34:01 +0200

    must reduce bed size instead of limits

--- Configuration.h
+++ Configuration.h
@@ -1697,14 +1697,14 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 235
+#define X_BED_SIZE 220
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE - 20
+#define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
